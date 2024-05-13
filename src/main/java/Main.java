6e6b7c222faf89055b1cc203718c980ad7b148e8
emanuellevitorializ <169068332+emanuellevitorializ@users.noreply.github.com>
2024-05-13 14:37:55 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    system.out.println("################")
+  }system.oit.println("################")
+
+    System.out.println(" cantores:Jorge Matheus");
+  siystem.out.println
+}
+System.out.println("álbum: os anjos cantam")
+  system.out.println("ano de lançamento: 2015")
+                     
+  system.out.println("")
+
+  system .out.println("Os Anjos Cantam é o segundo álbum de estúdio da dupla sertaneja Jorge & Mateus, lançado em 16 de março de 2015 pela Som Livre.[1] O repertório é formado por canções românticas que ressaltam a essência da dupla.")
